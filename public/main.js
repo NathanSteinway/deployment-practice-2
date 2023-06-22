9d3ac0d9b44ae93744749cc923bde33f0ba36ead
ganbari <steinway.nathan@gmail.com>
2023-06-22 14:57:46 -0500

    hmmm

--- public/main.js
+++ public/main.js
@@ -1,12 +1,15 @@
 const schnoz = document.getElementById('pushBtn')
+const schnozImg = document.getElementByClassName('img')
 const schnozPush = document.querySelector('button')
 
-const welcome = evt =>{
+const push = evt =>{
 
-    schnoz.src = "https://cdn.discordapp.com/attachments/600925351329136651/1120755561248608357/chrome_mk82XG1TqJ.png"
+    let schnozImgSrc = schnozImg.src
+
+    schnozImgSrc.textContent = "https://cdn.discordapp.com/attachments/600925351329136651/1120755561248608357/chrome_mk82XG1TqJ.png"
 
     alert('How could you?')
 
 }
 
-schnozPush.addEventListener('click', welcome)
+schnozPush.addEventListener('click', push)
